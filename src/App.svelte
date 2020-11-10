e56bf7fd5fdaa7218c938f17dbed94a90a3c295a
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-11-10 12:19:33 +0100

    design

--- src/App.svelte
+++ src/App.svelte
@@ -13,17 +13,16 @@ const cancel= (y)=>{
 
 <main>
  <table>
-  
-    <tr> 
-       <td><input type="text"  bind:value ={name} colspan="4" readonly="true"></td>        
-
   <tr>
-        <td> <button on:click= {()=>{namechanger('1')}}>1</button></td>
+     <th><input type="text"  bind:value ={name} colspan="4" readonly="true">  </th>      
+</tr>
+  <tr>
+       <td><button on:click= {()=>{namechanger('1')}}>1</button></td>
         <td> <button on:click= {()=>{namechanger('2')}}>2</button></td>
         <td> <button on:click= {()=>{namechanger('3')}}>3</button></td>
         <td><button on:click= {()=>{namechanger('+')}}>+</button></td> 
         
-    </tr>
+</tr>
    
     <tr>
         <td> <button on:click= {()=>{namechanger('4')}}>4</button></td>
@@ -50,5 +49,61 @@ const cancel= (y)=>{
 </main>
 
 <style>
-	
+	main{
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    }
+    table{
+       margin: auto;
+  background-color: black;
+  width: 29.5rem;
+  max-width: 29.5rem;
+  height: 32.5rem;
+  text-align: center;
+  border-radius: 0.4rem;
+  padding-right: 1rem;
+    }
+button{
+  outline: 0;
+  position: relative;
+  left: 0.5rem;
+  top: 0.5rem;
+  border: 0;
+  color: gray;
+  background-color: #fff;
+  border-radius: 0.4rem;
+  width: 6rem;
+  height: 5rem;
+  float: left;
+  margin: 5px;
+  font-size: 20px;
+  box-shadow: 0 0.4rem rgba(0,0,0,0.2);
+  margin-bottom: 1.5rem;
+}
+button:active {
+  top: 0.4rem;
+  border: 0 solid #000;
+  color: #495069;
+  background-color: #fff;
+  border-radius: 0.4rem;
+  width: 6rem;
+  height: 5rem;
+  float: left;
+  margin: 0.5rem;
+  font-size: 2rem;
+  box-shadow: none;
+}
+input{
+     width: 265px;
+  max-width: 270px;
+  font-size: 26px;
+  text-align: right;
+  background-color: #bcbd95;
+  float: left;
+}
+td{
+    width: 7rem!important
+}
 </style>
